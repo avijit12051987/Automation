623e96761ad6bf2362987181e4cf28ab77883556
SONAVYUMAN <SONAVYUMAN@SONAVYUMAN-PC>
2017-07-29 22:51:16 +0530

    added selenium jars and chrome driver

--- mavenIntro/src/main/java/AccessXmlMaven.java
+++ mavenIntro/src/main/java/AccessXmlMaven.java
@@ -3,11 +3,16 @@ package main.java;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AccessXmlMaven {
 
@@ -15,9 +20,11 @@ public class AccessXmlMaven {
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Inside main");
-		AccessXmlMaven.writeIntoExcel("Record.xlsx");
+		//AccessXmlMaven.writeIntoExcel("Record.xlsx");
 		System.out.println("Test 1");
 		System.out.println("Test 2");
+		AccessXmlMaven accessXmlMaven = new AccessXmlMaven();
+		accessXmlMaven.test();
 
 	}
 	public static void writeIntoExcel(String file) throws FileNotFoundException, IOException
@@ -31,6 +38,15 @@ public class AccessXmlMaven {
 			book.write(new FileOutputStream(file)); 
 			book.close(); 
 		}
+	public void test()
+	{
+		System.setProperty("webdriver.chrome.driver", "D://Users//SONAVYUMAN//git//mavenIntro//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demoqa.com/frames-and-windows/");
+		driver.findElement(By.xpath(".//*[@id='tabs-1']/div/p/a")).click();
+		//driver.close();
+		driver.quit();
+	}
 
 	
 
